8e2f143d450f8d4d9af7e35178017c3eee431a5e
Suyena Society <email@suyena.com>
2025-10-29 21:26:28 +0600

    Header section nearly done in showcase prototype.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
-import './App.css'
+import logo  from './assets/suyena-logo.png'
+import './App.scss'
 
 function App() {
 
   return (
     <>
-    <h1>Suyena Society</h1>
+    <header>
+      <div>
+        <img src={logo} alt="Suyena Logo" />
+      <h1>Suyena Society</h1>
+      </div>
+      <nav>
+        <ul>
+          <li><a href="#about">About Us</a></li>
+          <li><a href="#goals" >Our Goals</a></li>
+          <li><a href="#contact">Contact Us</a></li>
+        </ul>
+      </nav>
+    </header>
     </>
   )
 }
