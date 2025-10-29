ee753d37f1430a3168860ce89f368fd10b1e6875
Suyena Society <email@suyena.com>
2025-10-29 20:32:08 +0600

    A little initialization for server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 import express from 'express'
+import dotenv  from  'dotenv'
 
+dotenv.config()
 const app = express()
+const PORT = process.env.PORT
 
 app.get('/', (req, res)=>{
     res.status(200).send("Hello")
 })
 
-app.listen(7445, (req, res)=>{
-    console.log("Hi")
+app.listen(PORT, (req, res)=>{
+    console.log(`Hi from server using port ${PORT}`)
+    console.log(`Access server locally using http://localhost:${PORT}`)
 })
